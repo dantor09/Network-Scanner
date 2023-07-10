6ec80e6622330c930ee0d7366be6706d8814bc8a
Daniel Torres <D_Torres39@outlook.com>
2023-07-10 19:03:09 +0000

    [Refactor]: isValidIP function refactored
    - network interface call from ipaddress module
      automatically checks if ip is valid
    
    - No need to implement solution from scratch to
      validate ipv4 addresses.

--- net_scanner.py
+++ net_scanner.py
@@ -3,14 +3,19 @@ import ipaddress  #Allows for IP address manipulation
 import sys
 import re
 
-def isValidIP(ip_address):
-    ipv4_regex = r'^(\d){1,3}\.(\d){1,3}\.(\d){1,3}\.(\d){1,3}/(\d){1,2}$'
-    valid = re.findall(ipv4_regex, ip_address)
+def isValidIP(ip_address): 
+    valid = []
+    try:
+        valid = ipaddress.ip_interface(ip_address)
+    except ValueError:
+        print("IP: ", ip_address, " is invalid.")
     return valid
-
+    
 ip_address = input("IP Address: ")
 
 while not isValidIP(ip_address):
-    print(ip_address, " is invalid.")
     ip_address = input("IP Address: ")
+
+# Create an interface
+ip_address = ipaddress.ip_interface(ip_address)
 
