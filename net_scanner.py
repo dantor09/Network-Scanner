8ae8839ecc693b992693847cdb95ff26e952d8d4
Daniel Torres <D_Torres39@outlook.com>
2023-07-10 19:23:36 +0000

    [Feat]: Ability to ping a network
    - Once a network is defined and the host
      bits are iterable the script will iterate
      through each host and attempt to ping with
      1 packet. (ping -c 1 ip)
    
    - Three buckets were made, one for accepted pings,
      another for timeout pings, and for refused pings
    
    - Datetime module imported for time recording.

--- net_scanner.py
+++ net_scanner.py
@@ -2,6 +2,8 @@ import subprocess #allows for command line execution
 import ipaddress  #Allows for IP address manipulation
 import sys
 import re
+import os 
+import datetime
 
 def isValidIP(ip_address): 
     valid = []
@@ -18,4 +20,31 @@ while not isValidIP(ip_address):
 
 # Create an interface
 ip_address = ipaddress.ip_interface(ip_address)
+
+# Get a handle on network
+network = ipaddress.ip_network(str(ip_address.network))
+
+
+ip_ping_response = []
+ip_ping_timeout = []
+ip_ping_refuse = []
+
+#iterate through ip in network
+for ip in network:
+    response = os.system("ping -c 1 " + str(ip))
+
+    if response == 0:
+        print("0 Connection accepted from ", str(ip))
+        ip_ping_response.append(ip)
+    elif response == 1: 
+        print("1 Timeout from ", str(ip))
+        ip_ping_timeout.append(ip)
+    else:
+        print("2 Refused from ", str(ip))
+        ip_ping_refuse.append(ip)
+    print(datetime.datetime.now())
+
+
+
+print(ip_ping_response)
 
