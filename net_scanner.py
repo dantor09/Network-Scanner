c710019bd0679d3a820af2f401a696bae76cdddc
Daniel Torres <D_Torres39@outlook.com>
2023-07-13 18:18:12 +0000

    [Feat]: indicate open/close tcp connections
    - The script will only test ports if the ping gets a connection
      response.
    
    - If a connection is accepted from a certain IP then TCP ports are
      tested for connection and are added to csv format. Open means TCP
      port was open and closed means TCP port was closed or something
      went wrong.
    
    - If a ping response is not observed then all TCP ports are assumed
      not open. I believe these connections can still be tested because
      some computers turn off or go "ghost mode" on the ping ICMP.
    
    - This is a sloppy implementation, future iterations will be cleaner
      and easier to follow. This was just to prove functionality.
    
    - This is still a work in progress

--- net_scanner.py
+++ net_scanner.py
@@ -6,9 +6,11 @@ import csv
 import socket
 
 ports = [19,21,22,23,25,80,110,137,138,139,143,179,389,43,445,902,903,993,995,1080,1433,3306,3389,5900]
+protocol = ["Chargen","FTP","SSH","Telnet","SMTP","HTTP","POP3","NETBIOS","NETBIOS","NETBIOS","IMAP","BGP","LDAP","HTTPS","Microsoft directory services","VMware ESX","VMware ESX","IMAP TLS/SSL","POP3 TLS/SSL","SOCKS proxy","Microsoft SQL server","MySQL server","Remote desktop protocol","VNC remote desktop"]
 
 def TCPConnect(ip):
-    for port in ports:
+    csv_rows =""
+    for index, port in enumerate(ports):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.settimeout(2) 
         result = sock.connect_ex((str(ip),port))
@@ -16,9 +18,15 @@ def TCPConnect(ip):
         # SUCCESSFU
         if result == 0:
             print ("Port: " + str(port) + " is open on " + str(ip))
+            csv_rows += "Open"
         else:
             print ("Port: " + str(port) + " is closed on " + str(ip))
+            csv_rows += "Closed"
         sock.close()
+        
+        if index != 23:
+            csv_rows+=","
+    return csv_rows
 
 def isValidIP(ip_address): 
     valid = []
@@ -42,18 +50,32 @@ csv = open("information.csv", "a")
 csv.write("Date/Time"+ "," + "Host" + "," + "Ping" + "\n")
 
 for ip in network:
+    start_time = datetime.datetime.now().replace(microsecond=0)
     response = os.system("ping -c 1 -W 5 " + str(ip) + " > /dev/null")
     pinged = "no"
-    
+    csv_row = ""
+        
     if response == 0:
         print("0 Connection accepted from ", str(ip))
         pinged = "yes"
-        TCPConnect(ip)
+        csv_row = TCPConnect(ip)
     elif response == 1: 
         print("1 Timeout from ", str(ip))
+        for index in range(24):
+            print(index)
+            if index != 23:
+                csv_row += "Closed,"
+            else:
+                csv_row += "Closed"
     else:
         print("2 Refused from ", str(ip))
-    
-    csv.write(str(datetime.datetime.now().replace(microsecond=0)) + "," + str(ip) + "," + str(pinged)+"\n")
+
+        for index in range(24):
+            print(index)
+            if index != 23:
+                csv_row += "Closed," 
+            else:
+                csv_row += "Closed"
+    csv.write(str(start_time) + "," + str(ip) + "," + str(pinged)+ "," +csv_row + "\n")
 
 csv.close()
