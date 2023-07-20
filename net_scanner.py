16fa19632e58625d242a161da718a8660c37b801
Daniel Torres <D_Torres39@outlook.com>
2023-07-20 20:39:35 +0000

    [Refactor]: CSV class is component
    - Previously, the Network class had too many
      responsabilities encapsulated in it.
    
    - Now there are additional classes and will be
      used as components in the network class to
      be more organized and modular
    
    - This is the first step towards separating out
      responsabilities to other classes

--- net_scanner.py
+++ net_scanner.py
@@ -7,19 +7,34 @@ import socket
 import pandas as pd 
 import pyodbc 
 
-protocol = ["Chargen","FTP","SSH","Telnet","SMTP","HTTP","POP3","NETBIOS","NETBIOS","NETBIOS","IMAP","BGP","LDAP","HTTPS","Microsoft directory services","VMware ESX","VMware ESX","IMAP TLS/SSL","POP3 TLS/SSL","SOCKS proxy","Microsoft SQL server","MySQL server","Remote desktop protocol","VNC remote desktop"]
+class DatabaseConnection:
+    pass
+
+class CSV:
+    def __init__(self,fileName):
+        self.df = pd.DataFrame(columns=["Date/Time","Host","Ping","TCP/19","TCP/21","TCP/22","TCP/23","TCP/25","TCP/80","TCP/110","TCP/137","TCP/138","TCP/139","TCP/143","TCP/179","TCP/389","TCP/443","TCP/445","TCP/902","TCP/903","TCP/993","TCP/995","TCP/1080","TCP/1433","TCP/3606","TCP/3389","TCP/5900"])
+        self.fileName = fileName
+        self.csvRows = []
+    
+    def write_to_dataframe(self):
+        self.df.loc[len(self.df)] = self.csvRows 
+
+        self.csvRows = []
+    
+    def write_to_csv(self):
+        self.df.to_csv(self.fileName,sep=",",index=False)
 
 class Network:
+    
+    def __init__(self, ipAddress, fileName):
+        self.csv = CSV(fileName)
+        print("This is the CSV: ")
+        print(self.csv)
 
-    def __init__(self, ipAddress):
         self.ipAddress = ipaddress.ip_interface(ipAddress)
         self.ipNetwork = ipaddress.ip_network(str(self.ipAddress.network))
-        self.csvColumns = "Date/Time,Host,Ping,TCP/19,TCP/21,TCP/22,TCP/23,TCP/25,TCP/80,TCP/110,TCP/137,TCP/138,TCP/139,TCP/143,TCP/179,TCP/389,TCP/443,TCP/445,TCP/902,TCP/903,TCP/993,TCP/995,TCP/1080,TCP/1433,TCP/3606,TCP/3389,TCP/5900\n"
-        self.csvRows = ""
-        self.csvColumnsExist = False
         self.ports = [19,21,22,23,25,80,110,137,138,139,143,179,389,443,445,902,903,993,995,1080,1433,3306,3389,5900]
-        self.fileName = "information.csv"
-    
+         
     @staticmethod
     def is_valid_ip(ipAddress):
         valid = []
@@ -31,20 +46,6 @@ class Network:
         
         return valid
     
-    def write_to_csv(self, fileName):
-        
-        self.fileName = fileName
-        csv = open(self.fileName, "w")
-        
-        fileSize = os.path.getsize("./" + self.fileName)
-       
-        # do not append the columns if they already exist 
-        if self.csvColumnsExist == False and fileSize == 0:
-            csv.write(self.csvColumns)
-            self.csvColumnsExist = True
-        csv.write(self.csvRows)
-        csv.close()
-
     def test_tcp(self, ip):
 
         for index, port in enumerate(self.ports):
@@ -55,27 +56,23 @@ class Network:
             # SUCCESSFU
             if result == 0:
                 print ("Port: " + str(port) + " is open on " + str(ip))
-                self.csvRows += "Open"
+                self.csv.csvRows.append("Open")
             else:
                 print ("Port: " + str(port) + " is closed on " + str(ip))
-                self.csvRows += "Closed"
+                self.csv.csvRows.append("Closed")
             sock.close()
-        
-            if index != 23:
-                self.csvRows +=","
-            elif index == 23:
-                self.csvRows +="\n"
-
+        self.csv.write_to_dataframe()
 
     def ping_network(self):       
-        self.csvRows = ""
+        self.csv.csvRows = []
 
         for ip in self.ipNetwork:
             
             startTime = datetime.datetime.now().replace(microsecond=0)
             response = os.system("ping -c 1 -W 5 " + str(ip) + " > /dev/null")
             pinged = "no"
-            self.csvRows += str(startTime) + "," + str(ip) + ","
+            self.csv.csvRows.append(str(startTime))
+            self.csv.csvRows.append(str(ip))
         
             if response == 0:
                 print("0 Connection accepted from ", str(ip))
@@ -87,10 +84,11 @@ class Network:
                 print("2 Refused from ", str(ip))
                 pinged = "no"
             
-            self.csvRows += pinged + ","
+            self.csv.csvRows.append(pinged)
             
             self.test_tcp(ip)
 
+        self.csv.write_to_csv()
 
     def connection(self):
         data = pd.read_csv(self.fileName,  sep=",")
@@ -154,9 +152,12 @@ ipAddress = input("IP Address: ")
 while not Network.is_valid_ip(ipAddress):
     ipAddress = input("IP Address: ")
 
-network1 = Network(ipAddress)
+network1 = Network(ipAddress,"network1.csv")
 
 network1.ping_network()
-network1.write_to_csv("information.csv")
 
-network1.connection()
+
+
+
+
+
