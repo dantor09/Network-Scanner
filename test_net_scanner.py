1e7190ede8773d8b75748a87a8ab5a209445b15a
Daniel Torres <D_Torres39@outlook.com>
2023-07-31 18:33:10 +0000

    [Refactor]: switch from unittest module to pytest
    - Officially using pytest instead of unittest module
      to unit test python scripts.
    
    - to run the tests simply type "pytest" in the command line

--- test_net_scanner.py
+++ test_net_scanner.py
@@ -1,14 +1,12 @@
-import unittest
 from net_scanner import Network
 
 
-class TestNetScanner(unittest.TestCase):
 
     # if function name does not start with test, the test will not run correctly
-    def test_get_network(self):
-        
-        """Test that the network address is correct for a given IP/CIDR"""
-        test_data = [
+def test_get_network():
+
+    """Test that the network address is correct for a given IP/CIDR"""
+    test_data = [
         ("221.8.159.37/28", "221.8.159.32"),
         ("96.179.53.57/23", "96.179.52.0"),
         ("47.16.101.151/17", "47.16.0.0"),
@@ -21,96 +19,95 @@ class TestNetScanner(unittest.TestCase):
         ("23.57.46.204/8", "23.0.0.0"),
         ]
 
-        for ip_cidr, expected_network in test_data:
-            dummyNetwork = Network(ip_cidr)
-            network = dummyNetwork.get_network()
-            self.assertEqual(network, expected_network)
+    for ip_cidr, expected_network in test_data:
+        dummyNetwork = Network(ip_cidr)
+        network = dummyNetwork.get_network()
+        assert network == expected_network
 
-    def test_get_octet_index(self):
-        
-        """Test that the octet index is correct for a given CIDR"""
-        cidr_octet_mapping = [
-        (list(range(1, 9)), 0),
-        (list(range(9, 17)), 1),
-        (list(range(17, 25)), 2),
-        (list(range(25, 33)), 3)
-        ]
+def test_get_octet_index():
 
-        for cidr_range, expected_octet_index in cidr_octet_mapping:
-            for CIDR in cidr_range:
-                dummyNetwork = Network("172.27.131.1/" + str(CIDR))
-                octetIndex = dummyNetwork.get_octet_index()
-                self.assertEqual(octetIndex, expected_octet_index)
+    """Test that the octet index is correct for a given CIDR"""
+    cidr_octet_mapping = [
+            (list(range(1, 9)), 0),
+            (list(range(9, 17)), 1),
+            (list(range(17, 25)), 2),
+            (list(range(25, 33)), 3)
+            ]
+
+    for cidr_range, expected_octet_index in cidr_octet_mapping:
+        for CIDR in cidr_range:
+            dummyNetwork = Network("172.27.131.1/" + str(CIDR))
+            octetIndex = dummyNetwork.get_octet_index()
+            assert octetIndex == expected_octet_index
+
+def test_decode_ip():
+
+    test_data = [
+                ("3633769907", "216.150.237.179"),
+                ("1758202136", "104.204.9.24"),
+                ("3573630627", "213.1.70.163"),
+                ("3468956643", "206.196.19.227"),
+                ("1511687121", "90.26.131.209"),
+                ("2944128678", "175.123.214.166"),
+                ("3277952363", "195.97.149.107"),
+                ("2525154599", "150.130.205.39"),
+                ("3544356109", "211.66.149.13"),
+                ("2198693254", "131.13.101.134")
+                ]
+
+    for ipInteger, expectedIP in test_data:
+        dummyNetwork = Network(expectedIP + "/1")
+        decodedIP = dummyNetwork.decode_ip(ipInteger)
+        assert decodedIP == expectedIP
+
+def test_get_ip_range():
+    """Test that the IP range is correct for a given IP. This should 
+    return the start integer of the network block the ip lands on 
+    and the broadcast(end) integer for the block too"""
     
-    def test_decode_ip(self):
+    test_data = ["221.8.159.37/28","96.179.53.57/23",
+                "47.16.101.151/17","71.42.102.58/20",
+                "2.217.42.73/19","82.251.71.209/15",
+                "52.124.252.192/13","158.241.19.144/23",
+                "108.216.184.244/28","23.57.46.204/8"
+                ]
 
-        test_data = [
-        ("3633769907", "216.150.237.179"),
-        ("1758202136", "104.204.9.24"),
-        ("3573630627", "213.1.70.163"),
-        ("3468956643", "206.196.19.227"),
-        ("1511687121", "90.26.131.209"),
-        ("2944128678", "175.123.214.166"),
-        ("3277952363", "195.97.149.107"),
-        ("2525154599", "150.130.205.39"),
-        ("3544356109", "211.66.149.13"),
-        ("2198693254", "131.13.101.134"),
-        ]
+    start_stop_data = [("3708329760", "3708329775"),
+                ("1622356992", "1622357503"),
+                ("789577728", "789610495"),
+                ("1193959424", "1193963519"),
+                ("47783936", "47792127"),
+                ("1392115712", "1392246783"),
+                ("880279552", "880803839"),
+                ("2666598912", "2666599423"),
+                ("1826142448", "1826142463"),
+                ("385875968", "402653183")
+                ]
 
-        for ipInteger, expectedIP in test_data:
-            dummyNetwork = Network(expectedIP + "/1")
-            decodedIP = dummyNetwork.decode_ip(ipInteger)
-            self.assertEqual(decodedIP, expectedIP)
+    for index, ip_cidr in enumerate(test_data):
+        dummyNetwork = Network(ip_cidr)
+        start, stop = dummyNetwork.get_ip_range()
+        expected_start, expected_stop = start_stop_data[index]
+        assert (start, stop) == (int(expected_start), int(expected_stop))
 
-    def test_get_ip_range(self):
-        """Test that the IP range is correct for a given IP. This should 
-            return the start integer of the network block the ip lands on 
-            and the broadcast(end) integer for the block too"""
-        test_data = ["221.8.159.37/28","96.179.53.57/23",
-                     "47.16.101.151/17","71.42.102.58/20",
-                     "2.217.42.73/19","82.251.71.209/15",
-                     "52.124.252.192/13","158.241.19.144/23",
-                     "108.216.184.244/28","23.57.46.204/8",
-                    ]
-
-        start_stop_data = [("3708329760", "3708329775"),
-                           ("1622356992", "1622357503"),
-                           ("789577728", "789610495"),
-                           ("1193959424", "1193963519"),
-                           ("47783936", "47792127"),
-                           ("1392115712", "1392246783"),
-                           ("880279552", "880803839"),
-                           ("2666598912", "2666599423"),
-                           ("1826142448", "1826142463"),
-                           ("385875968", "402653183")
-                          ]
-        
-        for index, ip_cidr in enumerate(test_data):
-            dummyNetwork = Network(ip_cidr)
-            start, stop = dummyNetwork.get_ip_range()
-            expected_start, expected_stop = start_stop_data[index]
-            self.assertEqual((start, stop),(int(expected_start), int(expected_stop)))
-        
-    def test_get_broadcast_ip(self):
+    def test_get_broadcast_ip():
         """Test that the broadcast address is correct for a given IP/CIDR"""
         test_data = [
-        ("221.8.159.37/28", "221.8.159.47"),
-        ("96.179.53.57/23", "96.179.53.255"),
-        ("47.16.101.151/17", "47.16.127.255"),
-        ("71.42.102.58/20", "71.42.111.255"),
-        ("2.217.42.73/19", "2.217.63.255"),
-        ("82.251.71.209/15", "82.251.255.255"),
-        ("52.124.252.192/13", "52.127.255.255"),
-        ("158.241.19.144/23", "158.241.19.255"),
-        ("108.216.184.244/28", "108.216.184.255"),
-        ("23.57.46.204/8", "23.255.255.255"),
-        ]
+                ("221.8.159.37/28", "221.8.159.47"),
+                ("96.179.53.57/23", "96.179.53.255"),
+                ("47.16.101.151/17", "47.16.127.255"),
+                ("71.42.102.58/20", "71.42.111.255"),
+                ("2.217.42.73/19", "2.217.63.255"),
+                ("82.251.71.209/15", "82.251.255.255"),
+                ("52.124.252.192/13", "52.127.255.255"),
+                ("158.241.19.144/23", "158.241.19.255"),
+                ("108.216.184.244/28", "108.216.184.255"),
+                ("23.57.46.204/8", "23.255.255.255")
+                ]
 
         for ip_cidr, expected_broadcast in test_data:
             dummyNetwork = Network(ip_cidr)
             broadcast = dummyNetwork.get_broadcast_ip()
-            self.assertEqual(broadcast, expected_broadcast)
+            assert broadcast == expected_broadcast
 
-if __name__ == '__main__':
-    unittest.main()
 
